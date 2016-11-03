acf5e217b8e8c5965ca5744d3c3c982041d712e2
sohelrana820 <me.sohelrana@gmail.com>
2016-11-03 12:46:54 +0600

    Organize the files

--- car-house/index.php
+++ car-house/index.php
@@ -5,10 +5,7 @@
  * @description: Returning an image as response by php.
  */
 
-var_dump($_REQUEST);
-
-
-/*// Open the image file with binary mood
+// Open the image file with binary mood
 $image = './mobile_friendly_checking.png';
 $fp = fopen($image, 'rb');
 
@@ -18,4 +15,4 @@ header("Content-Length: " . filesize($image));
 
 // dumping the image and the stop the script
 fpassthru($fp);
-exit;*/
+exit;
