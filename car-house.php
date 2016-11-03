4546a5b84dbc52f4f3f0643c00c2ec14607d2a76
sohelrana820 <me.sohelrana@gmail.com>
2016-11-03 12:43:18 +0600

    Initialize commit

---
+++ car-house.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @Author: Sohel Rana <sohelrana820>
+ * @Author URI: http://blog.sohelrana.me
+ * @description: Returning an image as response by php.
+ */
+
+var_dump($_REQUEST);
+
+
+/*// Open the image file with binary mood
+$image = './mobile_friendly_checking.png';
+$fp = fopen($image, 'rb');
+
+// send the right headers
+header("Content-Type: image/png");
+header("Content-Length: " . filesize($image));
+
+// dumping the image and the stop the script
+fpassthru($fp);
+exit;*/
