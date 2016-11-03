5a83f1c63195f7c7d958a2c906fed635df5d4259
sohelrana820 <me.sohelrana@gmail.com>
2016-11-03 14:11:16 +0600

    Email is ready!

--- car-house/mail.php
+++ car-house/mail.php
@@ -1,10 +1,36 @@
 <?php
 $counter = 0;
-$file = fopen("data.csv","r");
-while(! feof($file)){
-    $ar=fgetcsv($file);
+$file = fopen("data.csv", "r");
+while (!feof($file)) {
+    $ar = fgetcsv($file);
     $counter++;
 }
 
-$message = "Today's visitor: ". $counter;
-mail('me.sohelrana@gmail.com', 'Car House - Visitor Report', $message);
+$message = "
+<html>
+<head>
+<title>Visitor Report</title>
+</head>
+<body>
+<p>Visitor Report</p>
+<table style='width:200px;'>
+<tr>
+<th style='width:100px;'>Date</th>
+<th>Visitor</th>
+</tr>
+<tr>
+<td>" . date('M d, Y') . "</td>
+<td>{$counter}</td>
+</tr>
+</table>
+
+<h2><a href='http://sohelrana.me/theme-tracking/car-house/data.csv'>Download Data</a></h2>
+
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+mail('me.sohelrana@gmail.com', 'Car House - Visitor Report', $message, $headers);
